5a7ce30136ddf6a7585726f897219a702d10f85a
JimmyOnTheCode <18689882+JimmyOnTheCode@users.noreply.github.com>
2022-11-26 00:40:11 +0100

    add snippet

---
+++ woocommerce/price-per-square-meter.php
@@ -0,0 +1,24 @@
+<?php
+//  Remove opening and closing php tags if implementing through WP Code Snippets plugin
+add_filter( 'woocommerce_get_price_html', 'add_price_per_square_meter_as_suffix' );
+function add_price_per_square_meter_as_suffix($price){
+	//	Null safety
+	if( (!isset($price)) or (empty($price)) or (is_null($price)) or ($price=="") or ($price ==0) ){
+		return $price;
+	}
+	
+	//	Get current product
+	$current_product = wc_get_product( get_the_ID() );
+	
+	//	Get product price and width
+	$product_price = $current_product->get_price();
+	$product_width = $current_product->get_width();
+	
+	//	Calculate price per square meter
+	$price_per_square_meter = round( ( ($product_price*1.19) / $product_width), 2);
+    
+	//	Add text after product price
+	$price_suffix = "($price_per_square_meter € je m2)";
+	return $price . ' ' .  $price_suffix;
+}
+>
